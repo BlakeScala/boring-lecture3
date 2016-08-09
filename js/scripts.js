285cdc4435c94662b5b0417aba88f1cbe7370028
Blake Scala and Seth Kendall <seth@>
2016-08-09 10:34:08 -0700

    add clickable text to add classes

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(function() {
+  $("#selectedpara1").click(function() {
+    $("#selectedpara1").addClass("clickable");
+  });
+});
+$(function() {
+  $("#selectedpara2").click(function() {
+    $("#selectedpara2").addClass("clickable");
+  });
+});
+$(function() {
+  $("#selectedpara3").click(function() {
+    $("#selectedpara3").addClass("clickable");
+  });
+});
+$(function() {
+  $("#selectedpara4").click(function() {
+    $("#selectedpara4").addClass("clickable");
+  });
+});
